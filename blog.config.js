5060a3daee261c1f0a3d9f86645e9b62898e8616
JiangnanXiaohe <jiangnanxiaohe@gmail.com>
2025-08-28 11:08:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '江南小和', 
-  BIO: process.env.NEXT_PUBLIC_BIO || '分享让品牌发光的营销思考', 
+  BIO: process.env.NEXT_PUBLIC_BIO || '分享让品牌发光的营销思考/广告策略与用户心理研究者', 
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jiangnanxiaohe.com', 
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '独立站, FB广告投放', 
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
