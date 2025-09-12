40ea8d52beb7c87cff1d0cc8bebb760e75b67d9f
JiangnanXiaohe <jiangnanxiaohe@gmail.com>
2025-09-12 16:13:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -58,7 +58,7 @@ const BLOG = {
   LAYOUT_SIDEBAR_REVERSE:
     process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
 
-  // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
+  // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
     'Hi，我是',
