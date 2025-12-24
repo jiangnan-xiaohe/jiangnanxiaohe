f8d905b7d0a67400e3c762ef3417d739cc3c9505
JiangnanXiaohe <jiangnanxiaohe@gmail.com>
2025-12-24 15:00:03 +0800

    Update SocialButton.js

--- themes/heo/components/SocialButton.js
+++ themes/heo/components/SocialButton.js
@@ -20,24 +20,59 @@ const SocialButton = () => {
   const CONTACT_YOUTUBE = siteConfig('CONTACT_YOUTUBE')
   const [showWechatQR, setShowWechatQR] = useState(false)
   const [showDouyinQR, setShowDouyinQR] = useState(false) 
+  const [showXHSQR, setShowXHSQR] = useState(false)
   
   const emailIcon = useRef(null)
 
   return (
     <div className='w-full justify-center flex-wrap flex'>
       <div className='space-x-8 text-3xl text-gray-600 dark:text-gray-300 '>
-          <a
-            target='_blank'
-            rel='noreferrer'
-            title='xiaohongshu'
-            href='https://xhslink.com/m/A3LHqn6y9ni'>
-            <img
-              src='https://cdn.jsdmirror.com/gh/88lin/picx-images-hosting@master/xiaohongshu.b9cpfgij9.svg'
-              alt='xiaohongshu'
-              className='inline-block transform hover:scale-125 duration-150 dark:hover:opacity-90 hover:opacity-90'
-              style={{ width: '1em', height: '1em', verticalAlign: '-0.125em' }}
-            />
-          </a>
+
+
+
+          <div
+            className='relative inline-flex'
+            onPointerEnter={(e) => {
+              if (e.pointerType === 'mouse') setShowXHSQR(true)
+            }}
+            onPointerLeave={(e) => {
+              if (e.pointerType === 'mouse') setShowXHSQR(false)
+            }}
+          >
+            <span
+              className='cursor-pointer transform transition-transform duration-150 hover:scale-125'
+              onClick={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                setShowXHSQR(v => !v)
+              }}
+            >
+        
+              <img
+                src='https://cdn.jsdmirror.com/npm/simple-icons@latest/icons/xiaohongshu.svg'
+                alt='xiaohongshu'
+                className='inline-block hover:opacity-80 dark:invert'
+                style={{ width: '1.2em', height: '1.2em', verticalAlign: '-0.2em' }}
+              />
+            </span>
+            {showXHSQR && (
+              <div className='absolute bottom-12 left-1/2 -translate-x-1/2 z-40'>
+                <div className='w-36 rounded-xl bg-white dark:bg-neutral-800 shadow-2xl ring-1 ring-black/5 flex flex-col items-center px-3 py-2'>
+                
+                  <img
+                    src='/images/xiaohongshu.JPG'
+                    alt='小红书二维码'
+                    className='block w-28 h-28 object-contain'
+                    draggable={false}
+                  />
+                  <div className='mt-1 text-[11px] leading-none text-gray-600 dark:text-gray-300 whitespace-nowrap'>
+                    扫码关注小红书
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+         
 
 
 
